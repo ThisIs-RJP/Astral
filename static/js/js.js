838c3ef5cec54935a4641e4c445db61bb90f1216
Rhodney James Paraiso <rjparaiso369@gmail.com>
2024-03-07 21:11:48 +0000

    Completed most of the main page

---
+++ static/js/js.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+
+    // $('.pauseSkip').click(function() {
+    //     $(this).toggleClass("toPause");
+    // });
+
+//     var div = document.getElementById("pauseButton");
+//     var changeToPaused = document.getElementById("toPause")
+//     let pausedOpacity = 0
+
+//     div.style.cursor = 'pointer';
+
+//     div.onclick = function() {
+//         var changeToPaused = document.getElementById("toPause")
+//         changeToPaused.style.opacity = 1
+
+//         var removeGo = document.getElementById("toGo")
+//         removeGo.style.opacity = "0"
+//     };
+
+//     div.onmouseover = function()
+//     {
+//         div.style.background = "#ff00ff";
+//     };
+    
+//     div.onmouseout = function() {
+//         div.style.background = "#ffffff";
+//     };
+}
