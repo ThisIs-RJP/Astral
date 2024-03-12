ec2e1d4a52cca34b3e4671f32efacb25c5711d90
ThisIs-RJP <rjparaiso369@gmail.com>
2024-03-12 20:26:41 +0000

    Finished designing the signup page

---
+++ templates/forms.py
@@ -0,0 +1,33 @@
+from django.db import transaction
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+######################## Creating a user
+
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField()
+
+    password1 = forms.CharField(
+        label= ("Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+
+    password2 = forms.CharField(
+        label= ("Confirm Password"),
+        widget=forms.PasswordInput,
+        strip=False,
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1' ,'password2' )
+        help_texts = {
+            'username': None,
+            'email': None,
+            'password1' : None,
+            'password2' : None,
+        }
--- templates/views.py
+++ templates/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import *
 from django.http import HttpResponse
+from templates.forms import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
-    return render(request, 'create.html')
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserForm()
+        return render(request, 'create.html', {'form': form})
